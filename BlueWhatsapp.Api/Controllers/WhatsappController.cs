985a54007a1a6b6f5006bb7cc87637e24e307423
Alvaro Guzman <aagn2807@gmail.com>
2025-03-31 10:04:30 -0400

    added token validation

--- BlueWhatsapp.Api/Controllers/WhatsappController.cs
+++ BlueWhatsapp.Api/Controllers/WhatsappController.cs
@@ -1,7 +1,7 @@
 ﻿using BlueWhatsapp.Api.Utils;
-using Microsoft.AspNetCore.Authorization;
+using BlueWhatsapp.Core.Logger;
 using Microsoft.AspNetCore.Mvc;
-using Triplex.Validations;
+using Quartz.Util;
 
 namespace BlueWhatsapp.Api.Controllers;
 
@@ -9,10 +9,39 @@ namespace BlueWhatsapp.Api.Controllers;
 [Route("api/[controller]")]
 public class WhatsappController : ControllerBase
 {
-    [HttpGet("")]
-    [LogAction]
-    public async Task<IActionResult> Get()
+    private readonly IAppLogger _logger;
+
+    public WhatsappController(IAppLogger logger)
     {
-        return Ok();
+        _logger = logger;
+    }
+
+    [HttpGet("health")]
+    public async Task<IActionResult> Health()
+    {
+        return Ok(new { healthy = true });
+    }
+
+    [HttpGet("verify-token")]
+    [LogAction]
+    public async Task<IActionResult> ValidateToken()
+    {
+        _logger.LogInfo("verify-token");
+
+        const string accessToken =
+            "EAAJZBUAn6wJcBADZCDnZCT0VEIqfScKb37HVn8ZAfPwl5LXZBm3r4ZBeE0ZCeVgf9DgZCY6YrXqLb1TxE9ZAZBfNPMjUuIiOHy18wxmTcOEtqWZAj6DTZAm1JvtEt9gYgJHOCySPrGYrJDYXmZBUPISDV6ezOPdI8MVE8fKwHq0ZBxzyjhVtq5eLQXQRljWIXPNHwYPqkJF";
+        string token = Request.Query["hub.verify_token"].ToString();
+        string challenge = Request.Query["hub.challenge"].ToString();
+
+        _logger.LogInfo($"token: {token}");
+        _logger.LogInfo($"challenge: {challenge}");
+
+        if (token.IsNullOrWhiteSpace() || token.IsNullOrWhiteSpace() || token is not accessToken)
+        {
+            _logger.LogError("Invalid token");
+            return BadRequest();
+        }
+
+        return Ok(challenge);
     }
 }
